3d018a5b17d6306cad99945fb8b72b6dfae36718
maja06 <majaradojicic06@gmail.com>
2024-06-15 13:32:03 +0100

    fix

--- Pmf0Scanner/location.h
+++ Pmf0Scanner/location.h
@@ -1,13 +1,14 @@
-// location.h
 #ifndef LOCATION_H
 #define LOCATION_H
 
 #ifndef YYLTYPE_IS_DECLARED
+
 typedef struct YYLTYPE {
     int first_line;
     int first_column;
     int last_line;
     int last_column;
+
 } YYLTYPE;
 #define YYLTYPE_IS_DECLARED 1
 #endif
